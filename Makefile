a9e68f245fb11c7d2cbbd97836d92fc7ca08fcad
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-03-08 15:20:34 -0800

    feat(acedb): reverted changes to forward X11 files to docker and added config for X11 forwarding through ssh

--- Makefile
+++ Makefile
@@ -1,7 +1,3 @@
 start-acedb:
 	xhost +local:root
 	docker-compose up -d --build acedb
-
-start-curation:
-	xhost +local:root
-	docker-compose up -d --build curation
--- curation/container_startup_setup.sh
+++ curation/container_startup_setup.sh
@@ -13,6 +13,13 @@ if [[ "${SSL_PORT}" -ne "443" ]]
 then echo "Listen ${SSL_PORT}" > /etc/apache2/additional_listeners.conf
 fi
 
+# X11 forwarding config for ssh
+mkdir /var/run/sshd
+sed -i "s/^.*PasswordAuthentication.*$/PasswordAuthentication no/" /etc/ssh/sshd_config
+sed -i "s/^.*X11Forwarding.*$/X11Forwarding yes/" /etc/ssh/sshd_config
+sed -i "s/^.*X11UseLocalhost.*$/X11UseLocalhost no/" /etc/ssh/sshd_config
+grep "^X11UseLocalhost" /etc/ssh/sshd_config || echo "X11UseLocalhost no" >> /etc/ssh/sshd_config
+
 # start services
 service apache2 start
 service ssh start
