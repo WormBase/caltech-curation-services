b6ab796b79dd17ec93298548356d441d74459a8d
Juancarlos <azurebrd@its.caltech.edu>
2023-03-15 19:16:39 -0700

    script to check if pmids in postgres are in the false positive list

---
+++ curation/scripts/agr_upload/pap_papers/check_fp.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+
+# WB_false_positive_pmids is aggregation of
+# /home/postgres/work/pgpopulation/wpa_papers/pmid_downloads/rejected_pmids
+# /home/postgres/work/pgpopulation/wpa_papers/pmid_downloads/removed_pmids
+# cat /home/postgres/work/pgpopulation/wpa_papers/pmid_downloads/rejected_pmids > blah
+# cat /home/postgres/work/pgpopulation/wpa_papers/pmid_downloads/removed_pmids >> blah
+# cat blah | sort | uniq > WB_false_positive_pmids
+# but some of those are still in our database.  This queries postgres for FP entries in DB
+# to clean them up.  2023 02 03
+
+
+use strict;
+use diagnostics;
+use DBI;
+use Encode qw( from_to is_utf8 );
+use Dotenv -load => '/usr/lib/.env';
+
+my $dbh = DBI->connect ( "dbi:Pg:dbname=$ENV{PSQL_DATABASE};host=$ENV{PSQL_HOST};port=$ENV{PSQL_PORT}", "$ENV{PSQL_USERNAME}", "$ENV{PSQL_PASSWORD}") or die "Cannot connect to database!\n";
+# my $dbh = DBI->connect ( "dbi:Pg:dbname=testdb", "", "") or die "Cannot connect to database!\n"; 
+my $result;
+
+my %pmid;
+$result = $dbh->prepare( "SELECT joinkey, pap_identifier FROM pap_identifier WHERE pap_identifier ~ 'pmid'" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  if ($row[0]) { 
+    $row[0] =~ s///g;
+    $row[1] =~ s///g;
+    $row[1] =~ s/pmid//g;
+    $pmid{$row[1]} = $row[0];
+  } # if ($row[0])
+} # while (@row = $result->fetchrow)
+
+my $infile = 'WB_false_positive_pmids';
+open (IN, "<$infile") or die "Cannot open $infile : $!";
+while (my $line = <IN>) {
+  chomp $line;
+  if ($pmid{$line}) { print qq(pmid$line $pmid{$line}\n); }
+}
+close (IN) or die "Cannot close $infile : $!";
+
+
+__END__
+
+my $dbh = DBI->connect ( "dbi:Pg:dbname=testdb;host=131.215.52.76", "", "") or die "Cannot connect to database!\n";	# for remote access
+
+my $result = $dbh->prepare( 'SELECT * FROM two_comment WHERE two_comment ~ ?' );
+$result->execute('elegans') or die "Cannot prepare statement: $DBI::errstr\n"; 
+
+$result->execute("doesn't") or die "Cannot prepare statement: $DBI::errstr\n"; 
+my $var = "doesn't";
+$result->execute($var) or die "Cannot prepare statement: $DBI::errstr\n"; 
+
+my $data = 'data';
+unless (is_utf8($data)) { from_to($data, "iso-8859-1", "utf8"); }
+
+my $result = $dbh->do( "DELETE FROM friend WHERE firstname = 'bl\"ah'" );
+(also do for INSERT and UPDATE if don't have a variable to interpolate with ? )
+
+can cache prepared SELECTs with $dbh->prepare_cached( &c. );
+
+if ($result->rows == 0) { print "No names matched.\n\n"; }	# test if no return
+
+$result->finish;	# allow reinitializing of statement handle (done with query)
+$dbh->disconnect;	# disconnect from DB
+
+http://209.85.173.132/search?q=cache:5CFTbTlhBGMJ:www.perl.com/pub/1999/10/DBI.html+dbi+prepare+execute&cd=4&hl=en&ct=clnk&gl=us
+
+interval stuff : 
+SELECT * FROM afp_passwd WHERE joinkey NOT IN (SELECT joinkey FROM afp_lasttouched) AND joinkey NOT IN (SELECT joinkey FROM cfp_curator) AND afp_timestamp < CURRENT_TIMESTAMP - interval '21 days' AND afp_timestamp > CURRENT_TIMESTAMP - interval '28 days';
+
+casting stuff to substring on other types :
+SELECT * FROM afp_passwd WHERE CAST (afp_timestamp AS TEXT) ~ '2009-05-14';
+
+to concatenate string to query result :
+  SELECT 'WBPaper' || joinkey FROM pap_identifier WHERE pap_identifier ~ 'pmid';
+to get :
+  SELECT DISTINCT(gop_paper_evidence) FROM gop_paper_evidence WHERE gop_paper_evidence NOT IN (SELECT 'WBPaper' || joinkey FROM pap_identifier WHERE pap_identifier ~ 'pmid') AND gop_paper_evidence != '';
+
