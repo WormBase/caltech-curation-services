124f6d9acd5881c75ce14e38fe4088d8fae1bf0b
Juancarlos <azurebrd@its.caltech.edu>
2023-03-27 16:36:30 -0700

    these 2 scripts output to file in local directory and to files/<something>.<date>

--- curation/scripts/citace_upload/dis_disease/check_disease_annotation.pl
+++ curation/scripts/citace_upload/dis_disease/check_disease_annotation.pl
@@ -2,16 +2,24 @@
 
 # check .ace file for Ranjana bassed on http://wiki.wormbase.org/index.php/OA_and_scripts_for_disease_data#Dumping_Disease_model_annotation_data
 # 2017 06 13
+#
+# Output to latest file locally, and also to files/<file>.date  2023 03 27
+
 
 use strict;
+use Jex;
 
 my $infile = $ARGV[0];
 
 unless ($infile) { die "Need to enter an inputfile ./check_disease_annotation.pl <filename>\n"; }
 
+my $date = &getSimpleDate();
+
 my $outfile = $infile . '.errors';
+my $outfile2 = 'files/' . $infile . '.errors.' . $date;
 
 open (OUT, ">$outfile") or die "Cannot create $outfile : $!";
+open (OU2, ">$outfile2") or die "Cannot create $outfile2 : $!";
 
 $/ = "";
 open (IN, "<$infile") or die "Cannot open $infile : $!";
@@ -21,14 +29,19 @@ while (my $entry = <IN>) {
   my $name = $lines[0];
   next unless ($name =~ m/Disease_model_annotation/);
   foreach my $tag (@regularTags) {
-    unless ($entry =~ m/$tag/) { print OUT qq($tag missing from $name\n); } }
+    unless ($entry =~ m/$tag/) { 
+      print OUT qq($tag missing from $name\n);
+      print OU2 qq($tag missing from $name\n); } }
   unless ( ($entry =~ m/Strain/) || ($entry =~ m/Variation/) || ($entry =~ m/Transgene/) || ($entry =~ m/Disease_relevant_gene/) ) {
-    print OUT qq(Strain + Variation + Transgene + Disease_relevant_gene missing from $name\n); }
+    print OUT qq(Strain + Variation + Transgene + Disease_relevant_gene missing from $name\n);
+    print OU2 qq(Strain + Variation + Transgene + Disease_relevant_gene missing from $name\n); }
   unless ($entry =~ m/Modifier_association_type/) {
     if ( ($entry =~ m/Modifier_transgene/) || ($entry =~ m/Modifier_variation/) || ($entry =~ m/Modifier_strain/) ||
          ($entry =~ m/Modifier_Gene/) || ($entry =~ m/Modifier_molecule/) || ($entry =~ m/Other_modifier/) ) {
-      print OUT qq(Modifier_association_type missing from $name\n); } }
+      print OUT qq(Modifier_association_type missing from $name\n);
+      print OU2 qq(Modifier_association_type missing from $name\n); } }
 }
 close (IN) or die "Cannot close $infile : $!";
 
 close (OUT) or die "Cannot close $outfile : $!";
+close (OU2) or die "Cannot close $outfile2 : $!";
--- curation/scripts/citace_upload/dis_disease/count_disease.pl
+++ curation/scripts/citace_upload/dis_disease/count_disease.pl
@@ -8,18 +8,28 @@
 # they show up in different pgid rows.
 # sort papers into either review or nonreview, and for any papers that show up on expmod or disrel
 # sort into review vs nonreview and print out counts and lists.  for Ranjana  2014 09 15
+#
+# send output to file disease_counts and also to files/disease_count.<date>  2023 03 27
 
 
 use strict;
 use diagnostics;
 use DBI;
 use Encode qw( from_to is_utf8 );
+use Jex;
 
 use Dotenv -load => '/usr/lib/.env';
 
 my $dbh = DBI->connect ( "dbi:Pg:dbname=$ENV{PSQL_DATABASE};host=$ENV{PSQL_HOST};port=$ENV{PSQL_PORT}", "$ENV{PSQL_USERNAME}", "$ENV{PSQL_PASSWORD}") or die "Cannot connect to database!\n";
 # my $dbh = DBI->connect ( "dbi:Pg:dbname=testdb", "", "") or die "Cannot connect to database!\n"; 
 my $result;
+
+my $date = &getSimpleDate();
+
+my $outfile = 'disease_counts';
+my $outfile2 = 'files/disease_counts.' . $date;
+
+my $output = '';
 
 my %hash;
 my $count_gene_entries = 0;
@@ -29,13 +39,13 @@ while (my @row = $result->fetchrow) {
   if ($row[0]) { 
     $count_gene_entries++;
     $hash{wbgene}{$row[1]}{$row[0]}++; } }
-print qq(No. of genes in dis_wbgene $count_gene_entries\n);
+$output .= qq(No. of genes in dis_wbgene $count_gene_entries\n);
 foreach my $gene (sort keys %{ $hash{wbgene} }) {
   my (@pgids) = sort keys %{ $hash{wbgene}{$gene} };
-  if (scalar @pgids > 1) { print qq($gene has mapping to pgids @pgids\n); }
+  if (scalar @pgids > 1) { $output .= qq($gene has mapping to pgids @pgids\n); }
 } # foreach my $gene (sort keys %{ $hash{wbgene} })
 my $count_unique_gene_entries = scalar keys %{ $hash{wbgene} };
-print qq(No. of unique genes in dis_wbgene $count_unique_gene_entries\n);
+$output .= qq(No. of unique genes in dis_wbgene $count_unique_gene_entries\n);
 
 my %doid; my $count_any_doid = 0;
 $result = $dbh->prepare( "SELECT * FROM dis_humandoid" );
@@ -44,9 +54,9 @@ while (my @row = $result->fetchrow) {
   if ($row[1]) { 
     my (@doid) = $row[1] =~ m/(DOID:\d+)/g;
     foreach (@doid) { $doid{$_}++; $count_any_doid++; } } }
-print qq(count all DO_terms $count_any_doid\n); 
+$output .= qq(count all DO_terms $count_any_doid\n); 
 my $count_unique_doid = scalar keys %doid;
-print qq(count unique DO_terms $count_unique_doid\n); 
+$output .= qq(count unique DO_terms $count_unique_doid\n); 
 
 my %unique_papers_any;
 my %unique_papers_expmod;
@@ -67,13 +77,13 @@ while (my @row = $result->fetchrow) {
       if ($papIsReview{$_}) { $unique_papers_review{$_}++; } else { $unique_papers_nonreview{$_}++; }
       $unique_papers_expmod{$_}++; $unique_papers_any{$_}++; } } }
 my $count_any_paperexpmod = scalar keys %unique_papers_expmod;
-print qq(count any paper exp mod $count_any_paperexpmod\n); 
+$output .= qq(count any paper exp mod $count_any_paperexpmod\n); 
 
 my $count_descriptions = 0;
 $result = $dbh->prepare( "SELECT COUNT(*) FROM dis_diseaserelevance" );
 $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
 while (my @row = $result->fetchrow) { if ($row[0]) { $count_descriptions = $row[0]; } }
-print qq(count disease descriptions : $count_descriptions\n);
+$output .= qq(count disease descriptions : $count_descriptions\n);
 
 my $count_genedisrel = 0;
 $result = $dbh->prepare( "SELECT * FROM dis_genedisrel" );
@@ -82,7 +92,7 @@ while (my @row = $result->fetchrow) {
   if ($row[1]) { 
     my (@genedisrel) = $row[1] =~ m/(\d+)/g;
     foreach (@genedisrel) { $count_genedisrel++; } } }
-print qq(count omim gene disrel : $count_genedisrel\n);
+$output .= qq(count omim gene disrel : $count_genedisrel\n);
 
 my %omim;
 $result = $dbh->prepare( "SELECT * FROM dis_dbdisrel" );
@@ -97,7 +107,7 @@ while (my @row = $result->fetchrow) {
     my (@omim) = $row[1] =~ m/(\d+)/g; foreach (@omim) { $omim{$row[0]}{$_}++; } } }
 my $count_omim_connected = 0;
 foreach my $pgid (sort keys %omim) { foreach my $omim (sort keys %{ $omim{$pgid} }) { $count_omim_connected++; } }
-print qq(Count omim diseases connected : $count_omim_connected\n);
+$output .= qq(Count omim diseases connected : $count_omim_connected\n);
 
 
 $result = $dbh->prepare( "SELECT * FROM dis_paperdisrel" );
@@ -109,21 +119,27 @@ while (my @row = $result->fetchrow) {
       if ($papIsReview{$_}) { $unique_papers_review{$_}++; } else { $unique_papers_nonreview{$_}++; }
       $unique_papers_disrel{$_}++; $unique_papers_any{$_}++; } } }
 my $count_any_paperdisrel = scalar keys %unique_papers_disrel;
-print qq(count any paper dis rel $count_any_paperdisrel\n); 
+$output .= qq(count any paper dis rel $count_any_paperdisrel\n); 
 
 
 my $count_unique_papers_any = scalar keys %unique_papers_any;
-print qq(count unique papers disrel or expmod : $count_unique_papers_any\n);
+$output .= qq(count unique papers disrel or expmod : $count_unique_papers_any\n);
 
 my $count_unique_papers_review = scalar keys %unique_papers_review;
-print qq(count unique papers disrel or expmod that are review : $count_unique_papers_review\n);
+$output .= qq(count unique papers disrel or expmod that are review : $count_unique_papers_review\n);
 
 my $count_unique_papers_nonreview = scalar keys %unique_papers_nonreview;
-print qq(count unique papers disrel or expmod that are not review : $count_unique_papers_nonreview\n);
+$output .= qq(count unique papers disrel or expmod that are not review : $count_unique_papers_nonreview\n);
 
-print qq(\nList of unique papers disrel or expmod that are review :\n);
-foreach my $paper (sort keys %unique_papers_review) { print "$paper\n"; }
+$output .= qq(\nList of unique papers disrel or expmod that are review :\n);
+foreach my $paper (sort keys %unique_papers_review) { $output .= "$paper\n"; }
 
-print qq(\nList of unique papers disrel or expmod that are not review :\n);
-foreach my $paper (sort keys %unique_papers_nonreview) { print "$paper\n"; }
+$output .= qq(\nList of unique papers disrel or expmod that are not review :\n);
+foreach my $paper (sort keys %unique_papers_nonreview) { $output .= "$paper\n"; }
 
+open (OUT, ">$outfile") or die "Cannot create $outfile : $!";
+open (OU2, ">$outfile2") or die "Cannot create $outfile2 : $!";
+print OUT $output;
+print OU2 $output;
+close (OUT) or die "Cannot create $outfile : $!";
+close (OU2) or die "Cannot create $outfile2 : $!";
