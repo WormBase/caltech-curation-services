72c1738f59b92a05aed3fff58483325f6196b577
Juancarlos <azurebrd@tazendra.caltech.edu>
2023-08-11 16:02:57 -0700

    new scripts

---
+++ curation/old_scripts_and_forms/postgres/work/pgpopulation/pap_papers/20230714_pap_gene_evidence_cleanup/check_afp_approved.pl
@@ -0,0 +1,147 @@
+#!/usr/bin/perl -w
+
+# Check what the data is for each afp_ datatype afp_approve column, to make sure we only have null / approved / rejected, and yes, that's the case.
+# 2023 10 11
+
+
+use strict;
+use diagnostics;
+use DBI;
+use Encode qw( from_to is_utf8 );
+
+my $dbh = DBI->connect ( "dbi:Pg:dbname=testdb", "", "") or die "Cannot connect to database!\n"; 
+my $result;
+
+my %datatypes;		# tie %datatypes, "Tie::IxHash";         # all allowed datatypes, was tieing to separate/order nonSvm vs Svm, but not anymore  2012 11 16
+my %datatypesAfpCfp;	# key datatype value is afp_ cfp_ tables's datatype to query (e.g. datatype 'blastomere' maps to 'cellfunc' for [ac]fp_cellfunc)
+my %premadeComments;
+
+my %cfp;
+my %curdata;
+
+&populateDatatypes();
+&populatePremadeComments();
+
+# &findEmptyCfpCurator();
+# &findEmptyCfpData();
+
+# &readCfp();
+# &readCurdata();
+
+&checkAfpApproved();
+
+sub checkAfpApproved {
+  foreach my $datatype (sort keys %datatypesAfpCfp) {
+    next if ($datatype eq 'catalyticact');
+    next if ($datatype eq 'chemphen');
+    next if ($datatype eq 'envpheno');
+    next if ($datatype eq 'rnaseq');
+    $result = $dbh->prepare( "SELECT afp_approve, COUNT(*) AS count FROM afp_$datatypesAfpCfp{$datatype} GROUP BY afp_approve ORDER BY count DESC" );
+    $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+    while (my @row = $result->fetchrow) {
+      $row[0] =~ s/\n/ /g;
+      print qq($datatype\t$row[1]\t$row[0]\n);
+} } }
+
+
+sub readCurdata {
+  $result = $dbh->prepare( "SELECT * FROM cur_curdata WHERE cur_site = 'caltech'" );
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+  while (my @row = $result->fetchrow) {
+    $curdata{$row[1]}{$row[0]}{data} = $row[4];
+    $curdata{$row[1]}{$row[0]}{curator} = $row[3];
+    $curdata{$row[1]}{$row[0]}{timestamp} = $row[7];
+} }
+
+sub readCfp {
+  foreach my $datatype (sort keys %datatypesAfpCfp) {
+    $result = $dbh->prepare( "SELECT * FROM cfp_$datatypesAfpCfp{$datatype}" );
+    $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+    while (my @row = $result->fetchrow) {
+      $cfp{$datatype}{$row[0]}{data} = $row[1];
+      $cfp{$datatype}{$row[0]}{curator} = $row[2];
+      $cfp{$datatype}{$row[0]}{timestamp} = $row[3];
+#       my $row = join"\t", @row;
+#       print qq($datatype\tcfp_$datatypesAfpCfp{$datatype}\t$row\n);
+} } }
+
+
+sub populateDatatypes {
+#   $result = $dbh->prepare( "SELECT DISTINCT(cur_datatype) FROM cur_svmdata" );        # switch from svm to nnc 2021 01 25
+  $result = $dbh->prepare( "SELECT DISTINCT(cur_datatype) FROM cur_nncdata" );
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n";
+  while (my @row = $result->fetchrow) { $datatypesAfpCfp{$row[0]} = $row[0]; }
+  $datatypesAfpCfp{'chemicals'}     = 'chemicals';              # added for Karen 2013 10 02
+  $datatypesAfpCfp{'blastomere'}    = 'cellfunc';
+  $datatypesAfpCfp{'exprmosaic'}    = 'siteaction';
+  $datatypesAfpCfp{'geneticmosaic'} = 'mosaic';
+  $datatypesAfpCfp{'laserablation'} = 'ablationdata';
+  $datatypesAfpCfp{'humandisease'}  = 'humdis';                 # added mapping to correct table 2018 05 17
+  $datatypesAfpCfp{'rnaseq'}        = 'rnaseq';                 # for new afp form 2018 10 31
+  $datatypesAfpCfp{'chemphen'}      = 'chemphen';               # for new afp form 2018 10 31
+  $datatypesAfpCfp{'envpheno'}      = 'envpheno';               # for new afp form 2018 10 31
+  $datatypesAfpCfp{'timeaction'}    = 'timeaction';             # for new afp form 2018 11 13
+  $datatypesAfpCfp{'siteaction'}    = 'siteaction';             # for new afp form 2018 11 13
+  foreach my $datatype (keys %datatypesAfpCfp) { $datatypes{$datatype}++; }
+  $datatypes{'geneticablation'}++;
+  $datatypes{'picture'}++;                      # for Daniela's pictures
+  $datatypes{'optogenetic'}++;                  # for Raymond's anatomy but without afp / cfp
+#   $datatypes{'rnaseq'}++;                     # for Kimberly Parasite  2016 07 12
+#   $datatypes{'proteomics'}++;                 # for Kimberly Parasite  2016 07 12
+#   $datatypes{'variants'}++;                   # for Kimberly Parasite  2016 07 12
+#   delete $datatypesAfpCfp{'catalyticact'};    # has svm but no afp / cfp      # afp got added, so cfp table also created.  2018 11 07
+  delete $datatypesAfpCfp{'expression_cluster'};        # has svm but no afp / cfp      # should have been removed 2017 07 08, fixed 2017 08 04
+  delete $datatypesAfpCfp{'genesymbol'};                # has svm but no afp / cfp      # added 2021 01 25
+  delete $datatypesAfpCfp{'transporter'};               # has svm but no afp / cfp      # added 2021 01 25
+  $result = $dbh->prepare( "SELECT DISTINCT(cur_datatype) FROM cur_strdata" );  # from string search data
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n";
+  while (my @row = $result->fetchrow) { $datatypes{$row[0]} = $row[0]; }
+} # sub populateDatatypes
+
+sub populatePremadeComments {
+  $premadeComments{"1"}  = "SVM Positive, Curation Negative";
+  $premadeComments{"2"}  = "C. elegans as heterologous expression system";
+  $premadeComments{"3"}  = "Curated for GO (by WB)";
+  $premadeComments{"4"}  = "Curated for GO (by GOA)";
+  $premadeComments{"5"}  = "Curated for GO (by IntAct)";
+  $premadeComments{"6"}  = "Curated for BioGRID (by WB)";
+  $premadeComments{"7"}  = "Curated for BioGRID (by BG)";
+  $premadeComments{"8"}  = "Curated for GO (by WB), Curated for BioGRID (by WB)";
+  $premadeComments{"9"}  = "Curated for GO (by WB), Curated for BioGRID (by BG)";
+  $premadeComments{"10"} = "Curated for GO (by GOA), Curated for BioGRID (by WB)";
+  $premadeComments{"11"} = "Curated for GO (by GOA), Curated for BioGRID (by BG)";
+  $premadeComments{"12"} = "Curated for GO (by IntAct), Curated for BioGRID (by WB)";
+  $premadeComments{"13"} = "Curated for GO (by IntAct), Curated for BioGRID (by BG)";
+  $premadeComments{"14"} = "Curation Negative, no Strain name given in paper";
+#   $premadeComments{"15"} = "No disease models";       # removed for Ranjana and removed from postgres.  2021 03 16
+  $premadeComments{"15"} = "Toxicology";
+  $premadeComments{"16"} = "Host-pathogen/virulence";
+  $premadeComments{"17"} = "Disease model";
+  $premadeComments{"18"} = "Non-genetic disease model";
+  $premadeComments{"19"} = "Genetic disease model";
+} # sub populatePremadeComments
+
+__END__
+
+my %manually;
+$result = $dbh->prepare( "SELECT * FROM pap_species WHERE pap_evidence ~ 'Manually_connected'" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  if ($row[0]) { 
+    my $key = $row[0] . '\t' . $row[1];
+    $manually{$key}++;
+  } # if ($row[0])
+} # while (@row = $result->fetchrow)
+
+my %confirmed;
+$result = $dbh->prepare( "SELECT * FROM pap_species WHERE pap_evidence ~ 'Curator_confirmed'" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  if ($row[0]) { 
+    my $key = $row[0] . '\t' . $row[1];
+    unless ($manually{$key}) {
+      print qq($key\n);
+    }
+  } # if ($row[0])
+} # while (@row = $result->fetchrow)
+
