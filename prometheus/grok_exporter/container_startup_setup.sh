f572c532f777ee91b6c531868d8a1e81b42ac246
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-11-13 15:08:34 -0800

    Add monitoring for Expression Cluster pipeline
    Added a new grok_exporter service for monitoring Expression Cluster extraction pipeline and set up a corresponding rule in Prometheus.
    
    This change was necessary to track the errors, runs with extracted data, processed papers, and successful runs of the Expression Cluster extraction pipeline. Furthermore, an alert for Expression Cluster Failure was added to Prometheus rule set to ensure proactive error detection. The tracking and alerts allow for improved overview, error detection and therefore increased stability of the pipeline.

--- prometheus/grok_exporter/container_startup_setup.sh
+++ prometheus/grok_exporter/container_startup_setup.sh
@@ -4,7 +4,9 @@
 
 mkdir -p /grok/log/afp
 mkdir -p /grok/log/ntt_extr/email_addr
+mkdir -p /grok/log/ntt_extr/expr_cluster
 
 touch /grok/log/ntt_extr/email_addr/email_ext_pipeline.log
+touch /grok/log/ntt_extr/expression_cluster/expr-cluster_ext_pipeline.log
 
 ./grok_exporter -config=/grok/config.yml
