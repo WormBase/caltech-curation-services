933fddd9eb27b2c0a7e7cfe0fe88d2980b27094c
Juancarlos <juancarlos@wormbase.org>
2024-03-12 17:19:39 -0700

    Figure out pap_identifiers becoming XREF at ABC, to see what regex should be there

---
+++ curation/scripts/pgpopulation/pap_papers/20240312_pap_identifier_patterns/get_pap_identifier_patterns.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/env perl
+
+# figure out pap identifier patterns that Kimberly cares about here   2024 03 12
+# https://agr-jira.atlassian.net/browse/SCRUM-3662?page=com.atlassian.jira.plugin.system.issuetabpanels%3Aall-tabpanel
+
+
+use strict;
+use diagnostics;
+use DBI;
+use Encode qw( from_to is_utf8 );
+# use JSON::Parse 'parse_json';
+# use JSON::XS;
+use JSON;
+use Jex;
+use Text::Unaccent;
+use Dotenv -load => '/usr/lib/.env';
+use utf8;
+
+binmode STDOUT, ':utf8';
+
+my $dbh = DBI->connect ( "dbi:Pg:dbname=$ENV{PSQL_DATABASE};host=$ENV{PSQL_HOST};port=$ENV{PSQL_PORT}", "$ENV{PSQL_USERNAME}", "$ENV{PSQL_PASSWORD}") or die "Cannot connect to database!\n";
+my $result;
+
+my %prefix;
+
+# check some subset of WM, but not all of it, full wm doesn't require 'wm' in the identifier.  see dump_agr_literature.pl for correct code.
+$result = $dbh->prepare( "SELECT * FROM pap_identifier WHERE joinkey IN (SELECT joinkey FROM pap_type WHERE pap_type = '3') ORDER BY joinkey, pap_order" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  if ($row[1] =~ m/wm/) {
+    unless ($row[1] =~ m/^[A-Za-z0-9]{6,13}$/) { print qq(@row\n); }
+  }
+}
+
+# check wbg pattern
+$result = $dbh->prepare( "SELECT * FROM pap_identifier WHERE pap_identifier ~ '^wbg' ORDER BY joinkey, pap_order" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  unless ($row[1] =~ m/^wbg\d{1,2}\.[A-Za-z0-9\.]{1,6}$/) { print qq(@row\n); }
+}
+
+# check cgc pattern
+$result = $dbh->prepare( "SELECT * FROM pap_identifier WHERE pap_identifier ~ '^cgc' ORDER BY joinkey, pap_order" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  unless ($row[1] =~ m/^cgc\d{1,4}$/) { print qq(@row\n); }
+}
+
+# Find prefixes
+# 
+# $result = $dbh->prepare( "SELECT * FROM pap_identifier ORDER BY joinkey, pap_order" );
+# $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+# while (my @row = $result->fetchrow) {
+# #   if ($row[0] % 1000 == 0) { print qq(@row\n); }
+#   next if ($row[1] =~ m/^\d{8}$/);
+#   my ($prefix) = $row[1] =~ m/^([A-Za-z]+)/;
+#   if ($prefix) { $prefix{$prefix}++; }
+#     else { print qq(NO PREFIX $row[1]\n); }
+# }
+# 
+# foreach my $prefix (sort keys %prefix) {
+#   print qq($prefix\t$prefix{$prefix}\n);
+# }
+
+
