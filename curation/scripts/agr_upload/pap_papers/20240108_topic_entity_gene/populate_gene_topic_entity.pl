f4943f913d148419755a0963dbe928bd613f5122
Juancarlos <juancarlos@wormbase.org>
2024-04-30 17:34:48 -0700

    Changed processing for https://agr-jira.atlassian.net/browse/SCRUM-3271  Still have some things to work out.  504 entries were sent to 4002 from test papers, and all were successful

--- curation/scripts/agr_upload/pap_papers/20240108_topic_entity_gene/populate_gene_topic_entity.pl
+++ curation/scripts/agr_upload/pap_papers/20240108_topic_entity_gene/populate_gene_topic_entity.pl
@@ -69,6 +69,7 @@ my $okta_token = &generateOktaToken();
 # my @wbpapers = qw( 00055090 );
 # my @wbpapers = qw( 00066031 );
 # my @wbpapers = qw( 00038491 00055090 );	# papers with lots of genes  2024 03 12
+# my @wbpapers = qw( 00003000 );
 my @wbpapers = qw( 00003000 00004455 00004952 00005199 00005707 00006103 00006202 00006320 00017095 00025176 00027230 00044280 00046571 00057043 00063127 00064676 00064771 00065877 00066211 );		# kimberly 2024 04 18 set
 
 # 00004952 00005199 00026609 00030933 00035427 00046571 00057043 00064676 
@@ -78,13 +79,24 @@ my %datatypesAfpCfp;
 my %datatypes;
 my %entitytypes;
 my %wbpToAgr;
+my %meetings;
 my %geneToTaxon;
+my %manConn;
 my %papGenePublished;
 
 my %chosenPapers;
 
+my %theHash;;
 my %infOther;
-my %curConf;
+my %curConfMan;
+my %curConfNoMan;
+my %perEvi;
+my %noEvi;
+my %cfp;
+my %afp;
+my %ack;
+my %absReadMeet;
+my %absReadNoMeet;
 
 # my $geneTopic = 'ATP:0000142';
 my $geneTopic = 'ATP:0000005';
@@ -95,10 +107,12 @@ foreach my $joinkey (@wbpapers) { $chosenPapers{$joinkey}++; }
 # $chosenPapers{all}++;
 
 &populateAbcXref();
+&populateMeetings();
 &populateGeneTaxon();
 &populatePapGene();
-&outputInfOther();
-&outputCurConf();
+# &outputInfOther();
+# &outputCurConf();
+&outputTheHash();
 
 if ($output_format eq 'json') {
   my $json = encode_json \@output_json;		# for single json file output
@@ -110,99 +124,173 @@ if ($output_format eq 'json') {
 # } 
 
 
-sub outputCurConf {
-  # old source
-  # my $source_type = 'professional_biocurator';
-  # my $source_method = 'paper_editor';
-  # my $source_id = &getSourceId($source_type, $source_method);
-  
-  my $source_evidence_assertion = 'ATP:0000036';
-  my $source_method = 'paper_editor_genes_curator';
-  my $data_provider = $mod;
-  my $secondary_data_provider = $mod;
-  my $source_id = &getSourceId($source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
-  my $timestamp = &getPgDate();
-  unless ($source_id) {
-    print qq(ERROR no source_id for $source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
-    return;
-  }
+# sub outputCurConf {
+#   # old source
+#   # my $source_type = 'professional_biocurator';
+#   # my $source_method = 'paper_editor';
+#   # my $source_id = &getSourceId($source_type, $source_method);
+#   
+#   my $source_evidence_assertion = 'ATP:0000036';
+#   my $source_method = 'paper_editor_genes_curator';
+#   my $data_provider = $mod;
+#   my $secondary_data_provider = $mod;
+#   my $source_id = &getSourceId($source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
+#   my $timestamp = &getPgDate();
+#   unless ($source_id) {
+#     print qq(ERROR no source_id for $source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
+#     return;
+#   }
+# 
+# #   { "source_type": "professional_biocurator", "source_method": "wormbase_oa", "evidence": "eco_string", "description": "caltech curation tools", "mod_abbreviation": "WB" }
+#   foreach my $joinkey (sort keys %curConf) {
+#     next unless ($chosenPapers{$joinkey} || $chosenPapers{all});
+#     foreach my $gene (sort keys %{ $curConf{$joinkey} }) {
+#       my %object;
+#       $object{'negated'}                    = FALSE;
+#       $object{'reference_curie'}            = $wbpToAgr{$joinkey};
+#       $object{'topic'}                      = $geneTopic;
+#       $object{'entity_type'}                = $entityType;
+#       $object{'entity_id_validation'}       = $entity_id_validation;
+#       $object{'topic_entity_tag_source_id'} = $source_id;
+#       $object{'entity'}                     = "WB:WBGene$gene";
+#       if ($geneToTaxon{$gene}) {
+#         $object{'species'}                  = $geneToTaxon{$gene}; }
+#       if ($papGenePublished{$joinkey}{$gene}) {
+#         $object{'entity_published_as'}      = $papGenePublished{$joinkey}{$gene}; }
+#       $object{'created_by'}                 = $curConf{$joinkey}{$gene}{curator};
+#       $object{'updated_by'}                 = $curConf{$joinkey}{$gene}{curator};
+#       $object{'date_created'}               = $curConf{$joinkey}{$gene}{timestamp};
+#       $object{'date_updated'}               = $curConf{$joinkey}{$gene}{timestamp};
+#       if ($output_format eq 'json') {
+#         push @output_json, \%object; }
+#       else {
+#         my $object_json = encode_json \%object;
+#         &createTag($object_json); }
+#   } }
+# }
+# 
+# sub outputInfOther {
+#   # my $source_type = 'script';
+#   # my $source_method = 'gene_paper_association_script';
+#   # my $source_id = &getSourceId($source_type, $source_method);
+# 
+#   my $source_evidence_assertion = 'ECO:0008021';
+#   my $source_method = 'paper_editor_genes_script';
+#   my $data_provider = $mod;
+#   my $secondary_data_provider = $mod;
+#   my $source_id = &getSourceId($source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
+#   my $timestamp = &getPgDate();
+#   unless ($source_id) {
+#     print qq(ERROR no source_id for $source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
+#     return;
+#   }
+# 
+# #   { "source_type": "professional_biocurator", "source_method": "wormbase_oa", "evidence": "eco_string", "description": "caltech curation tools", "mod_abbreviation": "WB" }
+#   foreach my $joinkey (sort keys %infOther) {
+#     next unless ($chosenPapers{$joinkey} || $chosenPapers{all});
+#     foreach my $gene (sort keys %{ $infOther{$joinkey} }) {
+#       my %object;
+#       $object{'negated'}                    = FALSE;
+#       $object{'reference_curie'}            = $wbpToAgr{$joinkey};
+#       $object{'topic'}                      = $geneTopic;
+#       $object{'entity_type'}                = $entityType;
+#       $object{'entity_id_validation'}       = $entity_id_validation;
+#       $object{'topic_entity_tag_source_id'} = $source_id;
+#       $object{'entity'}                     = "WB:WBGene$gene";
+#       if ($geneToTaxon{$gene}) {
+#         $object{'species'}                  = $geneToTaxon{$gene}; }
+# # TODO  entity_published_as  and  note are source specific
+#       if ($papGenePublished{$joinkey}{$gene}) {
+#         $object{'entity_published_as'}      = $papGenePublished{$joinkey}{$gene}; }
+#       if ($infOther{$joinkey}{$gene}{note}) {
+#         $object{'note'}                     = $infOther{$joinkey}{$gene}{note}; }
+#       $object{'created_by'}                 = $infOther{$joinkey}{$gene}{curator};
+#       $object{'updated_by'}                 = $infOther{$joinkey}{$gene}{curator};
+#       $object{'date_created'}               = $infOther{$joinkey}{$gene}{timestamp};
+#       $object{'date_updated'}               = $infOther{$joinkey}{$gene}{timestamp};
+#       if ($output_format eq 'json') {
+#         push @output_json, \%object; }
+#       else {
+#         my $object_json = encode_json \%object;
+#         &createTag($object_json); }
+#   } }
+# }
 
-#   { "source_type": "professional_biocurator", "source_method": "wormbase_oa", "evidence": "eco_string", "description": "caltech curation tools", "mod_abbreviation": "WB" }
-  foreach my $joinkey (sort keys %curConf) {
-    next unless ($chosenPapers{$joinkey} || $chosenPapers{all});
-    foreach my $gene (sort keys %{ $curConf{$joinkey} }) {
-      my %object;
-      $object{'negated'}                    = FALSE;
-      $object{'reference_curie'}            = $wbpToAgr{$joinkey};
-      $object{'topic'}                      = $geneTopic;
-      $object{'entity_type'}                = $entityType;
-      $object{'entity_id_validation'}       = $entity_id_validation;
-      $object{'topic_entity_tag_source_id'} = $source_id;
-      $object{'entity'}                     = "WB:WBGene$gene";
-      if ($geneToTaxon{$gene}) {
-        $object{'species'}                  = $geneToTaxon{$gene}; }
-      if ($papGenePublished{$joinkey}{$gene}) {
-        $object{'entity_published_as'}      = $papGenePublished{$joinkey}{$gene}; }
-      $object{'created_by'}                 = $curConf{$joinkey}{$gene}{curator};
-      $object{'updated_by'}                 = $curConf{$joinkey}{$gene}{curator};
-      $object{'date_created'}               = $curConf{$joinkey}{$gene}{timestamp};
-      $object{'date_updated'}               = $curConf{$joinkey}{$gene}{timestamp};
-      if ($output_format eq 'json') {
-        push @output_json, \%object; }
-      else {
-        my $object_json = encode_json \%object;
-        &createTag($object_json); }
-  } }
-}
 
-sub outputInfOther {
+sub outputTheHash {
   # my $source_type = 'script';
   # my $source_method = 'gene_paper_association_script';
   # my $source_id = &getSourceId($source_type, $source_method);
 
-  my $source_evidence_assertion = 'ECO:0008021';
-  my $source_method = 'paper_editor_genes_script';
-  my $data_provider = $mod;
-  my $secondary_data_provider = $mod;
-  my $source_id = &getSourceId($source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
-  my $timestamp = &getPgDate();
-  unless ($source_id) {
-    print qq(ERROR no source_id for $source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
-    return;
+  my %datatypeToSourceId;
+
+  foreach my $datatype (sort keys %theHash) {
+    my $source_evidence_assertion = 'ECO:0008021';
+    my $source_method = 'paper_editor_genes_script';
+    my $data_provider = $mod;
+    my $secondary_data_provider = $mod;
+    if ($datatype eq 'noEvi')              { $source_evidence_assertion = 'ECO:0006151'; $source_method = 'unknown'; }
+      elsif ($datatype eq 'infOther')      { $source_evidence_assertion = 'ECO:0008021'; $source_method = 'script_gene'; }
+      elsif ($datatype eq 'curConfNoMan')  { $source_evidence_assertion = 'ATP:0000036'; $source_method = 'genes_curator'; }
+      elsif ($datatype eq 'curConfMan')    { $source_evidence_assertion = 'ATP:0000036'; $source_method = 'paper_editor_genes_curator'; }
+      elsif ($datatype eq 'perEvi')        { $source_evidence_assertion = 'ATP:0000035'; $source_method = 'author_first_pass'; }
+      elsif ($datatype eq 'cfp')           { $source_evidence_assertion = 'ATP:0000036'; $source_method = 'genes_curator'; }
+      elsif ($datatype eq 'afp')           { $source_evidence_assertion = 'ATP:0000035'; $source_method = 'author_first_pass'; }
+      elsif ($datatype eq 'ack')           { $source_evidence_assertion = 'ATP:0000035'; $source_method = 'ACKnowledge_form'; }
+      elsif ($datatype eq 'absReadMeet')   { $source_evidence_assertion = 'ECO:0008021'; $source_method = 'script_gene_meeting_abstract'; }
+      elsif ($datatype eq 'absReadNoMeet') { $source_evidence_assertion = 'ECO:0008021'; $source_method = 'paper_editor_genes_script'; }
+    my $source_id = &getSourceId($source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
+    unless ($source_id) {
+      print qq(ERROR no source_id for $source_evidence_assertion, $source_method, $data_provider, $secondary_data_provider);
+      return;
+    }
+    $datatypeToSourceId{$datatype} = $source_id;
+    # print qq($source_id\t$datatype\n);
   }
 
-#   { "source_type": "professional_biocurator", "source_method": "wormbase_oa", "evidence": "eco_string", "description": "caltech curation tools", "mod_abbreviation": "WB" }
-  foreach my $joinkey (sort keys %infOther) {
-    next unless ($chosenPapers{$joinkey} || $chosenPapers{all});
-    foreach my $gene (sort keys %{ $infOther{$joinkey} }) {
-      my %object;
-      $object{'negated'}                    = FALSE;
-      $object{'reference_curie'}            = $wbpToAgr{$joinkey};
-      $object{'topic'}                      = $geneTopic;
-      $object{'entity_type'}                = $entityType;
-      $object{'entity_id_validation'}       = $entity_id_validation;
-      $object{'topic_entity_tag_source_id'} = $source_id;
-      $object{'entity'}                     = "WB:WBGene$gene";
-      if ($geneToTaxon{$gene}) {
-        $object{'species'}                  = $geneToTaxon{$gene}; }
-# TODO  entity_published_as  and  note are source specific
-      if ($papGenePublished{$joinkey}{$gene}) {
-        $object{'entity_published_as'}      = $papGenePublished{$joinkey}{$gene}; }
-      if ($infOther{$joinkey}{$gene}{note}) {
-        $object{'note'}                     = $infOther{$joinkey}{$gene}{note}; }
-      $object{'created_by'}                 = $infOther{$joinkey}{$gene}{curator};
-      $object{'updated_by'}                 = $infOther{$joinkey}{$gene}{curator};
-      $object{'date_created'}               = $infOther{$joinkey}{$gene}{timestamp};
-      $object{'date_updated'}               = $infOther{$joinkey}{$gene}{timestamp};
-      if ($output_format eq 'json') {
-        push @output_json, \%object; }
-      else {
-        my $object_json = encode_json \%object;
-        &createTag($object_json); }
-  } }
-}
+  foreach my $datatype (sort keys %theHash) {
+    foreach my $joinkey (sort keys %{ $theHash{$datatype} }) {
+      next unless ($chosenPapers{$joinkey} || $chosenPapers{all});
+      foreach my $gene (sort keys %{ $theHash{$datatype}{$joinkey} }) {
+        my %object;
+        $object{'negated'}                    = FALSE;
+        $object{'reference_curie'}            = $wbpToAgr{$joinkey};
+        $object{'topic'}                      = $geneTopic;
+        $object{'entity_type'}                = $entityType;
+        $object{'entity_id_validation'}       = $entity_id_validation;
+        $object{'topic_entity_tag_source_id'} = $datatypeToSourceId{$datatype};
+        $object{'entity'}                     = "WB:WBGene$gene";
+        if ($geneToTaxon{$gene}) {
+          $object{'species'}                  = $geneToTaxon{$gene}; }
+        if ( ($datatype eq 'curConfMan') && ($papGenePublished{$joinkey}{$gene}) ) {
+          my $published_as = join' | ', @{ $papGenePublished{$joinkey}{$gene} }; 
+          $object{'entity_published_as'}      = $published_as; }
+        if ($theHash{$datatype}{$joinkey}{$gene}{note}) {
+          my $note = join' | ', @{ $theHash{$datatype}{$joinkey}{$gene}{note} };
+          $object{'note'}                     = $note; }
+        $object{'created_by'}                 = $theHash{$datatype}{$joinkey}{$gene}{curator};
+        $object{'updated_by'}                 = $theHash{$datatype}{$joinkey}{$gene}{curator};
+        $object{'date_created'}               = $theHash{$datatype}{$joinkey}{$gene}{timestamp};
+        $object{'date_updated'}               = $theHash{$datatype}{$joinkey}{$gene}{timestamp};
+        if ($output_format eq 'json') {
+          push @output_json, \%object; }
+        else {
+          my $object_json = encode_json \%object;
+          &createTag($object_json); }
+    } }
+} }
+
 
 sub populatePapGene {
+  $result = $dbh->prepare( "SELECT joinkey, pap_gene, pap_timestamp, pap_curator, pap_evidence FROM pap_gene WHERE pap_evidence ~ 'Manually_connected'" );
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n";
+  while (my @row = $result->fetchrow) {
+    next unless ($chosenPapers{$row[0]} || $chosenPapers{all});
+    next unless ($row[1]);
+    my ($joinkey, $gene, $ts, $two, $evi) = @row;
+    if ($evi =~ m/Manually_connected.*"(.*?)"/) {
+      $manConn{$joinkey}{$gene} = $1; }
+  }
   $result = $dbh->prepare( "SELECT joinkey, pap_gene, pap_timestamp, pap_curator, pap_evidence FROM pap_gene" );
   $result->execute() or die "Cannot prepare statement: $DBI::errstr\n";
   while (my @row = $result->fetchrow) {
@@ -213,18 +301,52 @@ sub populatePapGene {
     if ($evi) { $evi =~ s/\n/ /g; $evi =~ s/ $//g; }
       else { $evi = ''; }
     if ($evi =~ m/Curator_confirmed.*(WBPerson\d+)/) {
-      $curConf{$joinkey}{$gene}{curator} = $1;
-      $curConf{$joinkey}{$gene}{timestamp} = $ts; }
+      if ($manConn{$joinkey}{$gene}) { 
+        $theHash{'curConfMan'}{$joinkey}{$gene}{curator} = $1;
+        $theHash{'curConfMan'}{$joinkey}{$gene}{timestamp} = $ts; }
+      else {
+        $theHash{'curConfNoMan'}{$joinkey}{$gene}{curator} = $1;
+        $theHash{'curConfNoMan'}{$joinkey}{$gene}{timestamp} = $ts; } }
+    elsif ($evi =~ m/Person_evidence.*(WBPerson\d+)/) {
+      $theHash{'perEvi'}{$joinkey}{$gene}{curator} = $1;
+      $theHash{'perEvi'}{$joinkey}{$gene}{timestamp} = $ts; }
     elsif ($evi =~ m/Inferred_automatically/) { 	# this has to be more specific later
       if ($evi =~ m/Inferred_automatically\s+"(Abstract read .*?)"/) {
-        $infOther{$joinkey}{$gene}{note} = $1; }
-      $infOther{$joinkey}{$gene}{curator} = $two;
-      $infOther{$joinkey}{$gene}{timestamp} = $ts; }
-    elsif ($evi =~ m/Published_as "(.*?)"/) {
-      $papGenePublished{$joinkey}{$gene} = $1; }
-    else {
-      # stuff without evidence
+        if ($meetings{$joinkey}) {
+          $theHash{'absReadMeet'}{$joinkey}{$gene}{curator} = $two;
+          $theHash{'absReadMeet'}{$joinkey}{$gene}{timestamp} = $ts;
+          push @{ $theHash{'absReadMeet'}{$joinkey}{$gene}{note} }, $1; }
+        else {
+          $theHash{'absReadNoMeet'}{$joinkey}{$gene}{curator} = $two;
+          $theHash{'absReadNoMeet'}{$joinkey}{$gene}{timestamp} = $ts;
+          push @{ $theHash{'absReadNoMeet'}{$joinkey}{$gene}{note} }, $1; } }
+      elsif ($evi =~ m/Inferred_automatically\s+"(from curator first pass .*?)"/) {
+        $theHash{'cfp'}{$joinkey}{$gene}{curator} = $two;
+        $theHash{'cfp'}{$joinkey}{$gene}{timestamp} = $ts;
+        push @{ $theHash{'cfp'}{$joinkey}{$gene}{note} }, $1; }
+      elsif ($evi =~ m/Inferred_automatically\s+"(from author first pass .*?)"/) {
+        my $tsdigits = &tsToDigits($ts);
+        if ($tsdigits < '20190322') {
+          $theHash{'afp'}{$joinkey}{$gene}{curator} = $two;
+          $theHash{'afp'}{$joinkey}{$gene}{timestamp} = $ts;
+          push @{ $theHash{'afp'}{$joinkey}{$gene}{note} }, $1; }
+        else {
+          $theHash{'ack'}{$joinkey}{$gene}{curator} = $two;
+          $theHash{'ack'}{$joinkey}{$gene}{timestamp} = $ts;
+          push @{ $theHash{'ack'}{$joinkey}{$gene}{note} }, $1; } }
+      else {
+        $theHash{'infOther'}{$joinkey}{$gene}{curator} = $two;
+        $theHash{'infOther'}{$joinkey}{$gene}{timestamp} = $ts; }
     }
+    elsif ($evi =~ m/Published_as "(.*?)"/) {
+      push @{ $papGenePublished{$joinkey}{$gene} }, $1; }
+    elsif ($evi =~ m/Manually_connected.*"(.*?)"/) {
+      $manConn{$joinkey}{$gene} = $1; }
+    elsif ($evi =~ m/Author_evidence/) {	# ignore these, should be removed from postgres
+      1; }
+    else {
+      $theHash{'noEvi'}{$joinkey}{$gene}{curator} = $two;
+      $theHash{'noEvi'}{$joinkey}{$gene}{timestamp} = $ts; }
 } }
 
 
@@ -247,9 +369,17 @@ sub populateGeneTaxon {
 
 
 
+sub populateMeetings {
+  $result = $dbh->prepare( "SELECT * FROM pap_type WHERE pap_type = '3';" );
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+  while (my @row = $result->fetchrow) { 
+    next unless ($chosenPapers{$row[0]} || $chosenPapers{all});
+    $meetings{$row[0]}++; }
+} # sub populateAbcXref
+
 sub populateAbcXref {
   $result = $dbh->prepare( "SELECT * FROM pap_identifier WHERE pap_identifier ~ 'AGRKB';" );
-  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 	# only molecules with papers are curated
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n";
   while (my @row = $result->fetchrow) { 
     next unless ($chosenPapers{$row[0]} || $chosenPapers{all});
     $wbpToAgr{$row[0]} = $row[1]; }
