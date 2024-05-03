4f616f301689718ae15c133c73db405ee7ceaebb
Juancarlos <azurebrd@tazendra.caltech.edu>
2024-05-03 15:38:51 -0700

    script to generate app_ data that is on tazendra but not on caltech dockerized

---
+++ curation/old_scripts_and_forms/postgres/work/pgpopulation/allele_phenotype/20240503_tazendra_transfer/generate_data_from_tazendra.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+
+# generate data from app_ that was added to tazendra and not to dockerized.
+# to be used with separate script that loads data on dockerized, using the files generated here.
+# 2024 05 03
+
+use strict;
+use diagnostics;
+use DBI;
+use Encode qw( from_to is_utf8 );
+
+my $dbh = DBI->connect ( "dbi:Pg:dbname=testdb", "", "") or die "Cannot connect to database!\n"; 
+my $result;
+
+my @tables;
+$result = $dbh->prepare( "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public' AND tablename ~ '^app_';" );
+$result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+while (my @row = $result->fetchrow) {
+  if ($row[0]) { 
+    $row[0] =~ s///g;
+    push @tables, $row[0];
+  } # if ($row[0])
+} # while (@row = $result->fetchrow)
+
+foreach my $table (@tables) {
+  $result = $dbh->prepare( "SELECT * FROM $table WHERE joinkey IN ('62670','62671','62672','62673','62674','62675','62676','62677','62678','62679','62680','62681','62682','62683','62684','62685','62686','62687','62688','62689','62690','62691','62692','62693','62694');" );
+  $result->execute() or die "Cannot prepare statement: $DBI::errstr\n"; 
+  while (my @row = $result->fetchrow) {
+    if ($row[0]) { 
+      my $data = join"\t", @row;
+      print qq($table\t$data\n);
+} } }
+
+__END__
+
+my $dbh = DBI->connect ( "dbi:Pg:dbname=testdb;host=131.215.52.76", "", "") or die "Cannot connect to database!\n";	# for remote access
+
+my $result = $dbh->prepare( 'SELECT * FROM two_comment WHERE two_comment ~ ?' );
+$result->execute('elegans') or die "Cannot prepare statement: $DBI::errstr\n"; 
+
+$result->execute("doesn't") or die "Cannot prepare statement: $DBI::errstr\n"; 
+my $var = "doesn't";
+$result->execute($var) or die "Cannot prepare statement: $DBI::errstr\n"; 
+
+my $data = 'data';
+unless (is_utf8($data)) { from_to($data, "iso-8859-1", "utf8"); }
+
+my $result = $dbh->do( "DELETE FROM friend WHERE firstname = 'bl\"ah'" );
+(also do for INSERT and UPDATE if don't have a variable to interpolate with ? )
+
+can cache prepared SELECTs with $dbh->prepare_cached( &c. );
+
+if ($result->rows == 0) { print "No names matched.\n\n"; }	# test if no return
+
+$result->finish;	# allow reinitializing of statement handle (done with query)
+$dbh->disconnect;	# disconnect from DB
+
+http://209.85.173.132/search?q=cache:5CFTbTlhBGMJ:www.perl.com/pub/1999/10/DBI.html+dbi+prepare+execute&cd=4&hl=en&ct=clnk&gl=us
+
+interval stuff : 
+SELECT * FROM afp_passwd WHERE joinkey NOT IN (SELECT joinkey FROM afp_lasttouched) AND joinkey NOT IN (SELECT joinkey FROM cfp_curator) AND afp_timestamp < CURRENT_TIMESTAMP - interval '21 days' AND afp_timestamp > CURRENT_TIMESTAMP - interval '28 days';
+
+casting stuff to substring on other types :
+SELECT * FROM afp_passwd WHERE CAST (afp_timestamp AS TEXT) ~ '2009-05-14';
+
+to concatenate string to query result :
+  SELECT 'WBPaper' || joinkey FROM pap_identifier WHERE pap_identifier ~ 'pmid';
+to get :
+  SELECT DISTINCT(gop_paper_evidence) FROM gop_paper_evidence WHERE gop_paper_evidence NOT IN (SELECT 'WBPaper' || joinkey FROM pap_identifier WHERE pap_identifier ~ 'pmid') AND gop_paper_evidence != '';
+
