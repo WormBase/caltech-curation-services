1d8c65cf23c7e2ce4732ba191618daef95137981
Juancarlos <juancarlos@wormbase.org>
2024-10-03 11:09:12 -0700

    script is looking up references by pmid at ABC and outputting any DOI.

--- curation/scripts/pgpopulation/pap_papers/20241002_doi_from_pmid/get_doi_from_pmid.pl
+++ curation/scripts/pgpopulation/pap_papers/20241002_doi_from_pmid/get_doi_from_pmid.pl
@@ -9,6 +9,14 @@
 # Dockerized, but found out  pmid2doi.org  doesn't exist anymore.  Updated it to get
 # batches of 200 for another tool, but that tool was for pmc only, so only has a 25%
 # success rate.  2024 10 02
+#
+# Kimberly would like to look these up by PMID from ABC, and populate WB with curator
+# being pubmed.  Basically anything that has a pmid at ABC would be considered data
+# from PubMed.  Makes sense.  We're not looking things up by WBPaper because there 
+# might be some conflict, or data might come from somewhere else and merged into 
+# WBPaper at ABC, though that would be unlikely.
+# For now, script is looking up references by pmid at ABC and outputting any DOI.
+# 2024 10 03
 
 
 use strict;
@@ -53,19 +61,28 @@ while (my @row = $result->fetchrow) {
   } # if ($row[0])
 } # while (@row = $result->fetchrow)
 
-print scalar @pmids . "\n";
-print qq(@pmids\n);
+my $count_wanted = scalar @pmids;
+# print qq(@pmids\n);
+
+my $count_matches = 0;
+foreach my $pmid (@pmids) {
+  my $doi = &getPmidFromAbc($pmid);
+  if ($doi) { $count_matches++; }
+  print qq($pmid\t$doi\n);
+}
+print qq(found $count_matches dois out of $count_wanted pmids\n);
+
 
 # my $max = 100;
-my $max = 200;
-while (scalar @pmids > 0) {
-  my @temp;
-  for (1 .. $max) { 
-    my $pmid = shift @pmids;
-    if ($pmid) { push @temp, $pmid; }
-  }
-  my $query = join",", @temp;
-print qq($query\n);
+# my $max = 200;
+# while (scalar @pmids > 0) {
+#   my @temp;
+#   for (1 .. $max) { 
+#     my $pmid = shift @pmids;
+#     if ($pmid) { push @temp, $pmid; }
+#   }
+#   my $query = join",", @temp;
+# print qq($query\n);
 #   my $url = 'http://www.pmid2doi.org/rest/json/batch/doi?pmids=[' . $query . ']';
 #   print "URL $url URL\n";
 # 
@@ -79,8 +96,31 @@ print qq($query\n);
 #     my $doi  = $entry->{"doi"};
 #     $pmidToDoi{$pmid} = $doi;
 #   } # foreach my $entry (@jsonArray)
-
+# 
 #   last;
+# }
+
+
+sub getPmidFromAbc {
+  my $pmid = shift;
+  # https://literature-rest.alliancegenome.org/reference/by_cross_reference/PMID%3A9221782
+  # take
+  #       "curie": "DOI:10.1523/JNEUROSCI.17-15-05843.1997",
+  # strip out the DOI:  and put 'doi' in front.
+  my $url = 'https://literature-rest.alliancegenome.org/reference/by_cross_reference/PMID:' . $pmid;
+  my $page_data = get $url;
+#   print "P $page_data P\n";
+  my $perl_scalar = $json->decode( $page_data );
+  my %hash = %$perl_scalar;
+  my $doi = '';
+  foreach my $xref (@{ $hash{cross_references} }) {
+    if ($$xref{curie_prefix} eq 'DOI') { 
+      my $doi = $$xref{curie};
+      $doi =~ s/DOI:/doi/;
+      return $doi;
+    }
+  }
+  return '';
 }
 
 __END__
